3a93b506c9b726b7254b5a112a142f66f028c98b
goranchik <goranchik@gmail.com>
2016-02-15 18:05:16 +0200

    initial

--- src/main/java/com/study/lab1/cache/UserCache.java
+++ src/main/java/com/study/lab1/cache/UserCache.java
@@ -1,10 +1,31 @@
 package com.study.lab1.cache;
 
 import com.study.lab1.dao.MockDataSource;
+import com.study.lab1.model.Account;
+import com.study.lab1.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created by dsk14 on 15.02.2016.
  */
+@Service
 public class UserCache {
+
+    @Autowired
     private MockDataSource dataSource;
+
+    private List<User> users = dataSource.getUsers();
+
+    public User getUser(long id) {
+        for (User user : users) {
+            if (user.getId() == id) {
+                return user;
+            }
+        }
+        return null;
+    }
+
 }
